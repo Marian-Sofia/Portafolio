0f508ce44b39b02e8e902cb59154a052482377ba
Marian-Sofia <sofiagutierrez0031@gmail.com>
2023-07-15 01:32:37 -0500

    animacion de las imagenes del proyecto

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -5,15 +5,25 @@ const Proyectos = () => {
         <div id='proyectos' className={style.contain}>
             <h1 className={style.title}>Mis Proyectos</h1>
             <div className={style.container}>
-                <h2 className={style.proyect}>Primer Proyecto</h2>
-                <img className={style.img} src='src\Img\random.jpg' />
-                <p className={style.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque molestias dicta eius, reprehenderit fugit consequuntur sed eveniet non commodi minus facere ex nulla earum tempore aliquam animi ad illum suscipit!</p>
+                <h2 className={style.proyect}>PI Dogs</h2>
             </div>
-            <div className={style.container}>
-                <h2 className={style.proyect}>Segundo Proyecto</h2>
-                <img className={style.img} src='src\Img\random.jpg' />
-                <p className={style.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque molestias dicta eius, reprehenderit fugit consequuntur sed eveniet non commodi minus facere ex nulla earum tempore aliquam animi ad illum suscipit!</p>
-            </div>
+            <div className={style.section}>
+                    <ul className={style.ul}>
+                        <li className={style.li}>
+                            <img className={style.img} src='src\Img\landing.png'/>
+                        </li>
+                        <li className={style.li}>
+                            <img className={style.img} src='src\Img\home.png'/>
+                        </li>
+                        <li className={style.li}>
+                            <img className={style.img} src='src\Img\search.png'/>
+                        </li>
+                        <li className={style.li}>
+                            <img className={style.img} src='src\Img\detail.png'/>
+                        </li>
+                    </ul>
+                </div>
+            <p className={style.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque molestias dicta eius, reprehenderit fugit consequuntur sed eveniet non commodi minus facere ex nulla earum tempore aliquam animi ad illum suscipit!</p>
         </div>
     )
 }
