587f6dbc45da88024d03e31cdbaf9af718fdbb2c
Marian-Sofia <sofiagutierrez0031@gmail.com>
2023-10-02 12:21:27 -0500

    responsive

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <h1 className={style.title}>BIENVENIDOS, YO SOY</h1> 
             <h2 className={style.name}>MARIAN SOFIA</h2>
             <h3 className={style.text}>Desarrolladora Web Full Stack, con residencia en Colombia. Soy una persona segura de sí misma, curiosa por naturaleza y siempre estoy trabajando para mejorar mis habilidades en la resolucion de un problema a la vez.</h3>
-            <a className={style.CV} href='https://drive.google.com/file/d/1ElZ27WB1hBMIIuBZrlZzJDIBkU6QjjhU/view?usp=sharing' download target='_blank' >MI CV</a>
+            <a className={style.CV} href='https://drive.google.com/file/d/1tBmLl4TxFokhV_hEB37AQz-Fmmjr5P1d/view?usp=sharing' download target='_blank' >MI CV</a>
         </div>
     )
 }
